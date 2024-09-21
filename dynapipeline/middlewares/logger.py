c0c58e5057227cbd45042e6120fb7cdcde240379
oldcorvus <moelcrow@gmail.com>
2024-09-21 20:37:13 +0200

    Add async support to LoggerMiddleware

--- dynapipeline/middlewares/logger.py
+++ dynapipeline/middlewares/logger.py
@@ -6,6 +6,7 @@ import logging
 from typing import Any, Dict, Optional
 
 from dynapipeline.core.base_middleware import BaseMiddleware
+from dynapipeline.core.context import BaseContext
 from dynapipeline.utils.types import T
 
 
@@ -16,6 +17,7 @@ class LoggerMiddleware(BaseMiddleware[T]):
 
     def __init__(
         self,
+        context: BaseContext,
         enabled: bool = True,
         description: str = "",
         logger: Optional[logging.Logger] = None,
@@ -23,13 +25,13 @@ class LoggerMiddleware(BaseMiddleware[T]):
         log_format: Optional[str] = None,
         extra_context: Optional[Dict[str, Any]] = None,
     ):
-        super().__init__(enabled, description)
+        super().__init__(context, enabled, description)
         self.logger = logger or logging.getLogger(__name__)
         self.log_level = log_level
         self.log_format = log_format or "{item}"
         self.extra_context = extra_context or {}
 
-    def _handle(self, item: T) -> T:
+    async def _handle(self, item: T) -> T:
         """
         Logs the item using the provided logger with custom formating
         """

35cb48c5d9da22ea3cc88172ce0d7b3e67a10bd1
oldcorvus <moelcrow@gmail.com>
2024-10-18 20:16:46 +0200

    Add SemaphoreExecutionStrategy

--- dynapipeline/execution/strategies.py
+++ dynapipeline/execution/strategies.py
@@ -27,3 +27,32 @@ class ConcurrentExecutionStrategy(ExecutionStrategy):
     async def execute(self, components: List[PipelineComponent], *args, **kwargs):
         tasks = [component.run(*args, **kwargs) for component in components]
         await asyncio.gather(*tasks)
+
+
+class SemaphoreExecutionStrategy(ExecutionStrategy):
+    """
+    A concrete execution strategy that uses a semaphore to limit the number of concurrent executions of pipeline components
+    """
+
+    def __init__(self, max_concurrent: int):
+        self.semaphore = asyncio.Semaphore(max_concurrent)
+
+    async def execute(self, components: List[PipelineComponent], *args, **kwargs):
+        """
+        Executes the pipeline components concurrently but limits the number of concurrent executions
+
+        """
+        tasks = [
+            self._run_with_semaphore(component, *args, **kwargs)
+            for component in components
+        ]
+        results = await asyncio.gather(*tasks)
+        return results
+
+    async def _run_with_semaphore(self, component: PipelineComponent, *args, **kwargs):
+        """
+        Wraps the execution of a component with a semaphore to limit concurrency
+
+        """
+        async with self.semaphore:
+            return await component.run(*args, **kwargs)

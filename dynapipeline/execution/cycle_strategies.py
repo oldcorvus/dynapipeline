d60c7ed063bfd2ff3beb8a499a0c3445ca1b7771
oldcorvus <moelcrow@gmail.com>
2024-10-18 20:26:16 +0200

    Add InfinitLoopStrategy

--- dynapipeline/execution/cycle_strategies.py
+++ dynapipeline/execution/cycle_strategies.py
@@ -1,5 +1,6 @@
 """Contains Strategies for specifying how often a component should run"""
-from typing import Any, Callable, List
+import asyncio
+from typing import Any, Callable, List, Optional
 
 from dynapipeline.execution.base import CycleStrategy
 from dynapipeline.pipelines.component import PipelineComponent
@@ -17,3 +18,26 @@ class OnceCycleStrategy(CycleStrategy):
     ) -> None:
         result = await execute_fn(components, *args, **kwargs)
         return result
+
+
+class InfinitLoopStrategy(CycleStrategy):
+    """Executes the group of components in a loop until event is set or pipeline stops"""
+
+    def __init__(self, event: Optional[asyncio.Event] = None) -> None:
+        self.event = event
+
+    async def run(
+        self,
+        execute_fn: Callable[[List[PipelineComponent], Any], Any],
+        components: List[PipelineComponent],
+        *args: Any,
+        **kwargs: Any
+    ) -> None:
+        if self.event is None:
+            while True:
+                await execute_fn(components, *args, **kwargs)
+
+        # If an event is provided loop until the event is set
+        else:
+            while not self.event.is_set():
+                await execute_fn(components, *args, **kwargs)
